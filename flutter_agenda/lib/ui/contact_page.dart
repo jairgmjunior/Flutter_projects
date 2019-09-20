f0f2dd13471e7e55a36c3bbd2d962f3afa24942f
Jair Gomes de M. Junior <jair.gmjunior@hotmail.com>
2019-09-20 05:06:24 -0300

    show agenda

--- flutter_agenda/lib/ui/contact_page.dart
+++ flutter_agenda/lib/ui/contact_page.dart
@@ -18,6 +18,8 @@ class _ContactPageState extends State<ContactPage> {
   final _emailController = new TextEditingController();
   final _phoneController = new TextEditingController();
 
+  final _focusName = new FocusNode();
+
   Contact _editContact;
   bool _userEdit = false;
 
@@ -37,68 +39,109 @@ class _ContactPageState extends State<ContactPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.redAccent,
-        title: Text(_editContact.name ?? "Novo Contato"),
-        centerTitle: true,
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: null,
-        child: Icon(Icons.save),
-        backgroundColor: Colors.redAccent,
-      ),
-      body: SingleChildScrollView(
-        padding: EdgeInsets.all(10.0),
-        child: Column(
-          children: <Widget>[
-            GestureDetector(
-              child: Container(
-                width: 140.0,
-                height: 140.0,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  image: DecorationImage(
-                    image: _editContact.img != null ?
-                    FileImage(File(_editContact.img)) :
-                    AssetImage("images/person.png"),
+    return WillPopScope(
+      onWillPop: _requestPop,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.redAccent,
+          title: Text(_editContact.name ?? "Novo Contato"),
+          centerTitle: true,
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            if(_editContact.name != null && _editContact.name.isNotEmpty){
+              Navigator.pop(context, _editContact);
+            }else{
+              FocusScope.of(context).requestFocus(_focusName);
+            }
+          },
+          child: Icon(Icons.save),
+          backgroundColor: Colors.redAccent,
+        ),
+        body: SingleChildScrollView(
+          padding: EdgeInsets.all(10.0),
+          child: Column(
+            children: <Widget>[
+              GestureDetector(
+                child: Container(
+                  width: 140.0,
+                  height: 140.0,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                      image: _editContact.img != null ?
+                      FileImage(File(_editContact.img)) :
+                      AssetImage("images/person.png"),
+                    ),
                   ),
                 ),
               ),
-            ),
-            TextField(
-              controller: _nameController,
-              decoration: InputDecoration(labelText: "Nome"),
-              onChanged: (text){
-                _userEdit = true;
-                setState(() {
-                  _editContact.name = text;
-                });
-              },
-            ),
-            TextField(
-              controller: _emailController,
-              decoration: InputDecoration(labelText: "Email"),
-              onChanged: (text){
-                _userEdit = true;
-                _editContact.email = text;
-              },
-              keyboardType: TextInputType.emailAddress,
-            ),
-            TextField(
-              controller: _phoneController,
-              decoration: InputDecoration(labelText: "Telefone"),
-              onChanged: (text){
-                _userEdit = true;
-                _editContact.phone = text;
-              },
-              keyboardType: TextInputType.phone,
-            ),
-          ],
+              TextField(
+                controller: _nameController,
+                focusNode: _focusName,
+                decoration: InputDecoration(labelText: "Nome"),
+                onChanged: (text){
+                  _userEdit = true;
+                  setState(() {
+                    _editContact.name = text;
+                  });
+                },
+              ),
+              TextField(
+                controller: _emailController,
+                decoration: InputDecoration(labelText: "Email"),
+                onChanged: (text){
+                  _userEdit = true;
+                  _editContact.email = text;
+                },
+                keyboardType: TextInputType.emailAddress,
+              ),
+              TextField(
+                controller: _phoneController,
+                decoration: InputDecoration(labelText: "Telefone"),
+                onChanged: (text){
+                  _userEdit = true;
+                  _editContact.phone = text;
+                },
+                keyboardType: TextInputType.phone,
+              ),
+            ],
+          ),
         ),
-      ),
+      )
     );
   }
 
+  Future<bool> _requestPop(){
+    if(_userEdit){
+      showDialog(context: context,
+        builder: (context){
+          return AlertDialog(
+            title: Text("Descartar Alterações?"),
+            content: Text("As alterações serão perdidas."),
+            actions: <Widget>[
+              FlatButton(
+                child: Text("Cancelar"),
+                onPressed: (){
+                  Navigator.pop(context);
+                },
+              ),
+              FlatButton(
+                child: Text("Sim"),
+                onPressed: (){
+                  Navigator.pop(context);
+                  Navigator.pop(context);
+                },
+              )
+            ],
+          );
+        }
+      );
+      return Future.value(false);
+    }
+    else{
+      return Future.value(true);
+    }
+  }
 
 }
--- flutter_agenda/lib/ui/home_page.dart
+++ flutter_agenda/lib/ui/home_page.dart
@@ -20,15 +20,7 @@ class _HomePageState extends State<HomePage> {
     super.initState();
     _getAllContacts();
   }
-
-  void _getAllContacts(){
-    helper.getAllContacts().then((list){
-      setState(() {
-        contacts = list;
-      });
-    });
-  }
-
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -109,12 +101,12 @@ class _HomePageState extends State<HomePage> {
         ),
       ),
       onTap: (){
-        _showContactPage(contact: contact);
+        _showOptions(context, contact);
       },
     );
   }
 
-  //parametro opcional
+  //{}parametro opcional
   void _showContactPage({Contact contact}) async {
     final recContact = await Navigator.push(
         context,
@@ -128,5 +120,72 @@ class _HomePageState extends State<HomePage> {
     }
 
     _getAllContacts();
+  }
+
+  void _getAllContacts(){
+    helper.getAllContacts().then((list){
+      setState(() {
+        contacts = list;
+      });
+    });
+  }
+  
+  void _showOptions(BuildContext context, Contact contact){
+    showModalBottomSheet(
+        context: context,
+        builder: (context){
+          return BottomSheet(
+            onClosing: (){},
+            builder: (context){
+              return Container(
+                padding: EdgeInsets.all(10.0),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.all(10.0),
+                      child: FlatButton(
+                        child: Text("Ligar",
+                          style: TextStyle(color: Colors.green, fontSize: 20.0),
+                        ),
+                        onPressed: (){
+
+                        },
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(10.0),
+                      child: FlatButton(
+                        child: Text("Editar",
+                          style: TextStyle(color: Colors.blueAccent, fontSize: 20.0),
+                        ),
+                        onPressed: (){
+                          Navigator.pop(context);
+                          _showContactPage(contact: contact);
+                        },
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(10.0),
+                      child: FlatButton(
+                        child: Text("Excluir",
+                          style: TextStyle(color: Colors.redAccent, fontSize: 20.0),
+                        ),
+                        onPressed: (){
+                          helper.deleteContact(contact.id);
+                          setState(() {
+                            contacts.remove(contact);
+                            Navigator.pop(context);
+                          });
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            },
+          );
+        }
+    );
   }
 }
