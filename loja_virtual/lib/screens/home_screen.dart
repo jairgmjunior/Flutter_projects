7a5b589a433e912b272e3fc8af8f25993e0a67ad
Jair Gomes de M. Junior <jair.gmjunior@hotmail.com>
2019-10-15 08:29:12 -0300

    Commit home screen

--- loja_virtual/lib/screens/home_screen.dart
+++ loja_virtual/lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:loja_virtual/tabs/home_tab.dart';
+import 'package:loja_virtual/widgets/custom_drawer.dart';
 
 class HomeScreen extends StatelessWidget {
 
@@ -11,7 +12,10 @@ class HomeScreen extends StatelessWidget {
       physics: NeverScrollableScrollPhysics(),
       controller: _pageController,
       children: <Widget>[
-        HomeTab(),
+        Scaffold(
+          body: HomeTab(),
+          drawer: CustomDrawer(),
+        )
 
 
       ],
--- loja_virtual/lib/tabs/home_tab.dart
+++ loja_virtual/lib/tabs/home_tab.dart
@@ -1,6 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 class HomeTab extends StatelessWidget {
   @override
@@ -19,18 +21,20 @@ class HomeTab extends StatelessWidget {
       ),
     );
 
+    Widget _buildFloatingTitle() => SliverAppBar(
+      floating: true ,
+      snap: true ,
+      backgroundColor: Colors.transparent ,
+      title: const Text("Novidades") ,
+      centerTitle: true ,
+    );
+
     return Stack(
       children: <Widget>[
         _buildBodyBack(),
         CustomScrollView(
           slivers: <Widget>[
-            SliverAppBar(
-              floating: true,
-              snap: true,
-              backgroundColor: Colors.transparent,
-              title: const Text("Novidades"),
-              centerTitle: true,
-            ),
+            _buildFloatingTitle(),
             FutureBuilder<QuerySnapshot>(
               future: Firestore.instance.collection("home").orderBy("pos").getDocuments(),
               builder: (context, snapshot){
@@ -45,13 +49,24 @@ class HomeTab extends StatelessWidget {
                     ),
                   );
                 }else{
-                  print(snapshot.data.documents.length);
-                  return SliverToBoxAdapter(
-                    child: Container(
-                      height: 200.0,
-                      alignment: Alignment.center,
-                      child: Container(),
-                    ),
+                  return SliverStaggeredGrid.count(
+                      crossAxisCount: 2,
+                      mainAxisSpacing: 1.0,
+                      crossAxisSpacing: 1.0,
+                      staggeredTiles: snapshot.data.documents.map(
+                        (doc){
+                          return StaggeredTile.count(doc.data["x"], doc.data["y"]);
+                        }
+                    ).toList(),
+                    children: snapshot.data.documents.map(
+                        (doc){
+                          return FadeInImage.memoryNetwork(
+                              placeholder: kTransparentImage,
+                              image: doc.data["image"],
+                            fit: BoxFit.cover,
+                          );
+                        }
+                    ).toList(),
                   );
                 }
               }
